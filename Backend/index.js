b863e68cc01e5561a4cc35bed7691ea62b4b252a
Himanshu Singh Dhapola <himanshu14sept@gmail.com>
2024-02-15 11:45:37 +0530

    changed cors link

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chat-room01.netlify.app",
     methods: ["GET", "POST"],
     credentials: true
   }
